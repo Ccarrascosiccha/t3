33e6fdec6b2bb491ff752a36acb3c65f1801b35c
Ccarrasco <67575360+Ccarrascosiccha@users.noreply.github.com>
2026-04-23 17:22:17 -0500

    Fix Core Data class mapping by removing objc alias

---
+++ t3/Models/MedicalRecord.swift
@@ -0,0 +1,12 @@
+import CoreData
+
+final class MedicalRecord: NSManagedObject {
+    @NSManaged var id: UUID?
+    @NSManaged var fullName: String?
+    @NSManaged var age: Int16
+    @NSManaged var weight: Double
+    @NSManaged var height: Double
+    @NSManaged var bloodPressure: String?
+    @NSManaged var comment: String?
+    @NSManaged var createdAt: Date?
+}
